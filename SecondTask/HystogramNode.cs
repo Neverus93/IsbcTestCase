c5482bbfc0e210320c00a7d9520a8629c5137838
Neverus93 <svsilent2054@gmail.com>
2022-02-23 06:01:49 +0300

    Продолжаю править алгоритм по второй задаче

---
+++ SecondTask/HystogramNode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecondTask
+{
+    public class HystogramNode
+    {
+        public int Index { get; set; }
+        public int Height { get; set; }
+
+        public HystogramNode(int index, int height)
+        {
+            Index = index;
+            Height = height;
+        }
+
+        public static HystogramNode[] GenerateHeightArray(int count, int maxValue)
+        {
+            Random random = new Random();
+            HystogramNode[] heightArray = new HystogramNode[count];
+            for (int i = 0; i < heightArray.Length; i++)
+            {
+                heightArray[i] = new HystogramNode(i + 1, random.Next(1, maxValue + 1));
+            }
+            return heightArray;
+        }
+
+        public static int MaximumRectangleArea(HystogramNode[] array)
+        {
+            int maxRectangleArea = 0;
+            Stack<HystogramNode> stack = new Stack<HystogramNode>();
+            HystogramNode nullElement = new HystogramNode(0, 0);
+            stack.Push(nullElement);
+            stack.Push(array[0]);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                if (array[i + 1].Height > stack.Peek().Height)
+                {
+                    stack.Push(array[i + 1]);
+                }
+                else
+                {
+                    while (array[i + 1].Height <= stack.Peek().Height)
+                    {
+                        maxRectangleArea = stack.Peek().Height * (i + 1);
+                        stack.Pop();
+                    }
+                }
+            }
+            return maxRectangleArea;
+        }
+    }
+}
--- SecondTask/Program.cs
+++ SecondTask/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SecondTask
 {
@@ -10,33 +7,19 @@ namespace SecondTask
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите количество прямоугольников в гистограмме: ");
-            int countOfRectangles = int.Parse(Console.ReadLine());
-            Console.Write("Введите максимальное значение высоты прямоугольника в гистограмме: ");
-            int rectangleMaxHeight = int.Parse(Console.ReadLine());
+            //Console.Write("Введите количество прямоугольников в гистограмме: ");
+            //int countOfRectangles = int.Parse(Console.ReadLine());
+            //Console.Write("Введите максимальное значение высоты прямоугольника в гистограмме: ");
+            //int rectangleMaxHeight = int.Parse(Console.ReadLine());
 
-            int[] hystogramHeightArray = GenerateHeightArray(countOfRectangles, rectangleMaxHeight);
-            int maxHystogramRectangleArea = MaximumRectangleArea(hystogramHeightArray);
+            int countOfRectangles = 4;
+            int rectangleMaxHeight = 4;
+
+
+            HystogramNode[] hystogramHeightArray = HystogramNode.GenerateHeightArray(countOfRectangles, rectangleMaxHeight);
+            int maxHystogramRectangleArea = HystogramNode.MaximumRectangleArea(hystogramHeightArray);
 
             Console.WriteLine($"Результат вычисления для гистограммы с {countOfRectangles} прямоугольниками и максимальной высотой шкалы, равной {rectangleMaxHeight}: {maxHystogramRectangleArea}");
-        }
-
-        static int[] GenerateHeightArray(int count, int maxValue)
-        {
-            Random random = new Random();
-            int[] heightArray = new int[count];
-            for(int i = 0; i < heightArray.Length; i++)
-            {
-                heightArray[i] = random.Next(1, maxValue + 1);
-            }
-            return heightArray;
-        }
-
-        //TODO
-        static int MaximumRectangleArea(int[] array)
-        {
-            Stack<int> stack;
-            return 0;
         }
     }
 }
