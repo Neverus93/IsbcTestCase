64b5dd5e70a28159767eef5ed395ad1d9f02c3f0
Neverus93 <svsilent2054@gmail.com>
2022-03-21 19:07:22 +0300

    Поправлен алгоритм формирования массива гистограммы. Поправлен алгоритм подсчёта максимальной площади, добавлен обратный подсчёт площади, чтобы иметь возможность "растягивать" элемент в обе стороны. Добавлен метод подсчёта минимального элемента в массиве.

--- SecondTask/HystogramNode.cs
+++ SecondTask/HystogramNode.cs
@@ -17,15 +17,16 @@ namespace SecondTask
             Random random = new Random();
             List<int> heightValues = new List<int>();
 
-            foreach(var e in heightValues)
+            heightValues.Add(0); // Элемент необходим для обратной проверки блоков массива
+            for (int i = 0; i < count; i++)
             {
                 heightValues.Add(random.Next(1, maxValue + 1));
             }
-            heightValues.Add(0);//Дополнительный элемент для алгоритма подсчёта
+            heightValues.Add(0); // Элемент необходим, чтобы в самом конце вытолкнуть все необходимые элементы из стека
 
             int[] heightValuesArray = heightValues.ToArray(); //Массив полученных высот прямоугольников гистограммы
 
-            HystogramNode[] heightArray = new HystogramNode[count + 1];
+            HystogramNode[] heightArray = new HystogramNode[count + 2];
 
             for (int i = 0; i < heightArray.Length; i++)
             {
@@ -36,48 +37,64 @@ namespace SecondTask
 
         public static int MaximumRectangleArea(HystogramNode[] array)
         {
-            int maxRectangleArea = 0; //объявляем переменную, в которую будет записан результат вычисления площади максимального прямоугольника, вписанного в стек
             int countOfHystogramElements = array.Length; //количество прямоугольников гистограммы
             Stack<HystogramNode> stack = new Stack<HystogramNode>(); //заводим стек из прямоугольников гистограммы
             HystogramNode minusOneElement = new HystogramNode(0, -1); //добавляем элемент с высотой -1, который никогда не будет вытолкнут из стека
             stack.Push(minusOneElement); //добавляем элемент высотой -1 в стек
-          
-            for(int i = 0; i < countOfHystogramElements; i++)
+            int minimumValueOfArrayElement = FindMinimumHystogramBlock(array);
+            int maxRectangleArea = minimumValueOfArrayElement * (countOfHystogramElements - 2);
+
+            for (int i = 0; i < countOfHystogramElements; i++)
             {
-                int result = 0;
-                if (array[i].NodeHeight > stack.Peek().NodeHeight)
+                if (array[i].NodeHeight >= stack.Peek().NodeHeight)
                 {
                     stack.Push(array[i]);
                 }
                 else
                 {
-                    if(array[i].NodeHeight == 0)
+                    while (array[i].NodeHeight < stack.Peek().NodeHeight)
                     {
-                        for (int j = 0; j < stack.Count; j++)
+                        int result = stack.Peek().NodeHeight * (i - stack.Peek().NodeIndex + 1);
+                        if(result > maxRectangleArea)
                         {
-                            result = stack.Pop().NodeHeight * (j + 1);
-                            if(result > maxRectangleArea)
-                            {
-                                maxRectangleArea = result;
-                            }
+                            maxRectangleArea = result;
                         }
+                        stack.Pop();
                     }
-                    else if(array[i].NodeHeight > 0)
+
+                    int index = 0;
+
+                    for (int j = i; array[j].NodeHeight >= array[i].NodeHeight; j--)
                     {
-                        for(int j = i; j >= array[i].)
+                        if(i == countOfHystogramElements - 1)
+                        {
+                            break;
+                        }
+                        int turnResult = array[i].NodeHeight * (index + 1);
+                        if(turnResult > maxRectangleArea)
+                        {
+                            maxRectangleArea = turnResult;
+                        }
+                        index++;
                     }
-                    //for(int j = 0; j > stack.Count; j++) //TODO Не заходит в цикл
-                    //{
-                    //    int result = stack.Pop().NodeHeight * (j - i + 1);
-                    //    if (result > maxRectangleArea)
-                    //    {
-                    //        maxRectangleArea = result;
-                    //    }
-                    //}
-                    //stack.Push(array[i]);
+                    stack.Push(array[i]);
                 }
             }
             return maxRectangleArea;
+        }
+
+        private static int FindMinimumHystogramBlock(HystogramNode[] array)
+        {
+            int result = array[1].NodeHeight;
+
+            for(int i = 1; i < array.Length - 1; i++)
+            {
+                if(array[i].NodeHeight < result)
+                {
+                    result = array[i].NodeHeight;
+                }
+            }
+            return result;
         }
 
         public override string ToString()
